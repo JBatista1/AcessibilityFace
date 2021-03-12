e6bbaf6cbb781783d51f6994e3e9b1f78cb64653
João Batista <joao.damasceno@bs2tecnologia.com.br>
2021-03-12 17:51:13 -0300

    Initial commit

---
+++ Example/AcessibilityFace/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AcessibilityFace
+//
+//  Created by j.batista.damasceno@icloud.com on 03/12/2021.
+//  Copyright (c) 2021 j.batista.damasceno@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
